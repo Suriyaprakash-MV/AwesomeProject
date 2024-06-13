428ccc0a447acfec9c5f6a9403fdcec2a342c6fd
Suriyaprakash-MV <mmvsuriyaprakash@gmail.com>
2024-06-13 11:52:22 +0530

    setup

--- App.tsx
+++ App.tsx
@@ -8,6 +8,7 @@
 import React from 'react';
 import type {PropsWithChildren} from 'react';
 import {
+  Pressable,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -25,93 +26,85 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+function App(): React.JSX.Element {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.mainView}>
+      <View style={styles.secondView}>
+        <View style={styles.tickholder}>
+          <Text style={styles.tick}>✓</Text>
+        </View>
+        <Text style={styles.heading}>Success</Text>
+        <Text style={styles.subHeading}>
+          Your message was sent successfully,{'\n'} please check your mail to
+          confirm.
+        </Text>
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonText}>Continue</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  mainView: {
+    height: '100%',
+    width: '100%',
+    backgroundColor: '#ffe5b4',
+    justifyContent: 'center',
+    alignContent: 'center',
+    alignItems: 'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  secondView: {
+    width: '90%',
+    height: '97%',
+    backgroundColor: 'white',
+    borderRadius: 40,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
+  tickholder: {
+    width: 200,
+    height: 200,
+    backgroundColor: '#ffe5b4',
+    alignSelf: 'center',
+    borderRadius: 100,
+    marginTop: 90,
+    justifyContent: 'center',
+    alignContent: 'center',
+    alignItems: 'center',
+  },
+  heading: {
+    fontSize: 50,
+    color: 'black',
+    alignSelf: 'center',
+    fontWeight: '800',
+    marginTop: 30,
+  },
+  subHeading: {
+    fontSize: 17,
+    color: 'grey',
+    alignSelf: 'center',
     fontWeight: '400',
+    marginTop: 10,
   },
-  highlight: {
-    fontWeight: '700',
+  button: {
+    backgroundColor: '#FF4500',
+    padding: 10,
+    borderRadius: 15,
+    height: 50,
+    width: 350,
+    alignItems: 'center',
+    marginLeft: 10,
+    marginTop: 310,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    alignItems: 'center',
+    fontWeight: '800',
+  },
+  tick: {
+    fontSize: 140,
+    color: '#FF4500',
   },
 });
 
